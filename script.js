a924fd8e18246cdb33d7c786d366457e8c92f79e
Rishita017 <Rishita17006@gmail.com>
2026-02-19 13:13:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function toggleMenu() {
   const menu = document.querySelector("nav ul");
-  menu.style.display =
-    menu.style.display === "flex" ? "none" : "flex";
+  menu.classList.toggle("active");
 }
-
